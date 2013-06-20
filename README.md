04ba2b6b76803e0e639176b62ad41e124778c59b
Nabil Hassein <nabil.hassein@gmail.com>
2013-06-20 13:36:09 -0400

    wrote code to serialize internal representation of bencoded strings back to bytestring format found in the specification; removed extraneous helper function; updated dependencies; updated README

--- README.md
+++ README.md
@@ -42,6 +42,10 @@ If you forget, your inferior ghci (or whatever) will be unable to find packages
 that were `cabal install`-ed in the local virtual environment,
 as ghci will instead search the normal environment.
 
+Also, be sure to `:set -XOverloadedStrings` in your `.ghci`.
+
+## contributing
+
 In terms of actually writing code: for now, I guess you should
 develop on a local branch and then send me a pull request.
 If you want to begin working on this project regularly, or even semi-regularly,
@@ -52,14 +56,6 @@ I'll gladly give commit access! Just ask.
 This isn't about bitcurry strictly, but if you have any ideas about better
 development practices, whether general or haskell-specific, please let me know!
 
-# useful resources and references
-- https://wiki.theory.org/BitTorrentSpecification
-- http://www.kristenwidman.com/blog/how-to-write-a-bittorrent-client-part-1/
-- http://www.kristenwidman.com/blog/how-to-write-a-bittorrent-client-part-2/
-- https://github.com/thomasballinger/bittorrent
-- https://github.com/astro/haskell-torrent
-- http://www.haskell.org/pipermail/haskell-cafe/2005-April/009638.html
-
 # TODO
 So far, I've written a datatype for bencoding,
 with associated code for parsing and serialization.
@@ -69,9 +65,21 @@ The next step I plan to tackle is [communicating with the tracker via HTTP](http
 After that, we can begin working on implementing peer-to-peer communication,
 and actually downloading files.
 
+As an additional feature not found in the specification, I hope to implement
+the ability to control the client via SMS. This has nothing in particular to do
+with Bittorrent.
+
 ## possible issues to revisit
 
 ### encoding -- ASCII vs. Unicode
 See https://en.wikipedia.org/wiki/Bencode#Encoding_algorithm
 
 "The specification does not deal with encoding of characters outside the ASCII set"
+
+# useful resources and references
+- https://wiki.theory.org/BitTorrentSpecification
+- http://www.kristenwidman.com/blog/how-to-write-a-bittorrent-client-part-1/
+- http://www.kristenwidman.com/blog/how-to-write-a-bittorrent-client-part-2/
+- https://github.com/thomasballinger/bittorrent
+- https://github.com/astro/haskell-torrent
+- http://www.haskell.org/pipermail/haskell-cafe/2005-April/009638.html
--- src/Bencode.hs
+++ src/Bencode.hs
@@ -1,60 +1,67 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Bencode where
 
 import           Control.Applicative ((<|>))
-import           Data.Attoparsec (Parser, word8, many', count, anyWord8)
+import           Data.Attoparsec (Parser, many', count, anyWord8, string)
 import           Data.Attoparsec.ByteString.Char8 (decimal, signed)
-import           Data.Char (ord)
-import           Data.URLEncoded
-import           Data.Serialize (Serialize)
-import           Data.Word8 (Word8)
-import           GHC.Generics (Generic)
+import           Data.ByteString.Char8 (pack)
 import qualified Data.ByteString as BS
 import qualified Data.Map        as Map
 
 
+-- see https://wiki.theory.org/BitTorrentSpecification#Bencoding
 data Bencode = BString BS.ByteString
              | BInt Integer
              | BList [Bencode]
              | BDict (Map.Map Bencode Bencode)
-             deriving (Show, Eq, Ord, Generic)
-instance Serialize Bencode
-instance URLEncode Bencode
+             deriving (Show, Eq, Ord)
+
+
+-- TODO: write some tests!
+bencode :: Bencode -> BS.ByteString
+bencode (BString str) = let n = pack . show $ BS.length str
+                        in  n `BS.append` ":" `BS.append` str
+bencode (BInt n)      = "i" `BS.append` pack (show n) `BS.append` "e"
+bencode (BList xs)    = "l" `BS.append` foldr BS.append "e" (map bencode xs)
+bencode (BDict dict)  = "d" `BS.append` helper (BDict dict) `BS.append` "e"
+  where helper (BDict hash) = let list = Map.toList hash
+                              in case list of
+                                []        -> ""
+                                (k, v):xs -> bencode k `BS.append`
+                                             bencode v `BS.append`
+                                             helper (BDict $ Map.fromList xs)
 
 
 parseBencode :: Parser Bencode
 parseBencode = parseString <|> parseInteger <|> parseList <|> parseDictionary
 
-charToWord8 :: Char -> Word8
-charToWord8 = fromIntegral . ord
-
 parseString :: Parser Bencode
 parseString = do
   n   <- decimal
-  _   <- word8 $ charToWord8 ':'
+  _   <- string ":"
   str <- count n anyWord8
   return $ BString $ BS.pack str
 
 parseInteger :: Parser Bencode
 parseInteger = do
-  _ <- word8 $ charToWord8 'i'
+  _ <- string "i"
   n <- signed decimal
-  _ <- word8 $ charToWord8 'e'
+  _ <- string "e"
   return $ BInt n
 
 parseList :: Parser Bencode
 parseList = do
-  _  <- word8 $ charToWord8 'l'
+  _  <- string "l"
   xs <- many' parseBencode
-  _  <- word8 $ charToWord8 'e'
+  _  <- string "e"
   return $ BList xs
 
 parseDictionary :: Parser Bencode
 parseDictionary = do
-  _  <- word8 $ charToWord8 'd'
+  _  <- string "d"
   xs <- many' parseHash
-  _  <- word8 $ charToWord8 'e'
+  _  <- string "e"
   return $ BDict $ Map.fromList xs
 
 parseHash :: Parser (Bencode, Bencode)
